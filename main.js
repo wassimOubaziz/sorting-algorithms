69cccfa2b9b7283dc56ca6eaf39d01484c67b105
Wassim Oubaziz <114287083+wassimOubaziz@users.noreply.github.com>
2024-01-04 12:06:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let sort = ``;
 const button1 = document.querySelector(".button.el1");
 const button2 = document.querySelector(".button.el2");
 const button3 = document.querySelector(".button.el3");
-et small = 0; // Declare the 'small' variable here
+let small = 0; // Declare the 'small' variable here
 let temp = arr[0]; // Declare the 'temp' variable here
 
 let arr = [5, 2, 3, 10, 6, 8, 4, 1];
